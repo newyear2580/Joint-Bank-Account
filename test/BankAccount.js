6dd7da46bdbb920b34dbcd5e0cf6388b6363fc49
newyear2580 <51331397+newyear2580@users.noreply.github.com>
2023-04-18 10:05:00 +0800

    Add files via upload

---
+++ test/BankAccount.js
@@ -0,0 +1,227 @@
+const {
+  loadFixture,
+} = require("@nomicfoundation/hardhat-network-helpers");
+const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
+const { expect } = require("chai");
+
+describe("BankAccount", function () {
+  async function deployBankAccount() {
+    const [addr0, addr1, addr2, addr3, addr4] = await ethers.getSigners() ;
+    // addr0, addr1, addr2, addr3, addr4 為 signer object
+
+    const BankAccount = await ethers.getContractFactory("BankAccount") ;
+    const bankAccount = await BankAccount.deploy() ;
+
+    return { bankAccount, addr0, addr1, addr2, addr3, addr4 } ;
+  }
+
+  async function deployBankAccountWithAccounts(
+    owners = 1,
+    deposit = 0,
+    withdrawAmounts = []
+  )
+  {
+    const { bankAccount, addr0, addr1, addr2, addr3, addr4 } = await loadFixture( deployBankAccount ) ;
+    let addresses = [] ;
+
+    if ( owners == 2 ) 
+      addresses = [addr1.address] ;
+    else if ( owners == 3 )
+      addresses = [addr1.address, addr2.address] ;
+    else if ( owners == 4 )
+      addresses = [addr1.address, addr2.address, addr3.address] ;
+    
+    await bankAccount.connect( addr0 ).createAccount( addresses ) ;
+
+    if ( deposit > 0 ) {
+      await bankAccount.connect( addr0 ).deposit( 0, { value: deposit.toString() } ) ;
+    } // if
+
+    for ( const withdrawAmount of withdrawAmounts ) {
+      await bankAccount.connect( addr0 ).requestWithdraw( 0, withdrawAmount ) ;
+    } // for
+
+    return { bankAccount, addr0, addr1, addr2, addr3, addr4 } ;
+  }
+
+  // 前面的 test case 不會影響到之後的測試
+  describe( "Deployment", () => {
+    it ( "Should deploy without error", async () => {
+      await loadFixture( deployBankAccount ) ;
+    } ) ;
+  } ) ;
+
+  describe( "Creating an account", () => {
+    it ( "should allow creating a single user account", async () => {
+      const { bankAccount, addr0 } = await loadFixture( deployBankAccount ) ;
+      await bankAccount.connect( addr0 ).createAccount( [] ) ;
+      const accounts = await bankAccount.connect( addr0 ).getAccounts() ;
+      expect( accounts.length ).to.equal( 1 ) ;
+    } ) ;
+
+    it ( "should allow creating a double user account", async () => {
+      const { bankAccount, addr0, addr1 } = await loadFixture( deployBankAccount ) ;
+      await bankAccount.connect( addr0 ).createAccount( [addr1.address] ) ;
+
+      const accounts0 = await bankAccount.connect( addr0 ).getAccounts() ;
+      expect( accounts0.length ).to.equal( 1 ) ;
+
+      const accounts1 = await bankAccount.connect( addr1 ).getAccounts() ;
+      expect( accounts1.length ).to.equal( 1 ) ;
+    } ) ;
+
+    it ( "should allow creating a triple user account", async () => {
+      const { bankAccount, addr0, addr1, addr2 } = await loadFixture( deployBankAccount ) ;
+      await bankAccount.connect( addr0 ).createAccount( [addr1.address, addr2.address] ) ;
+
+      const accounts0 = await bankAccount.connect( addr0 ).getAccounts() ;
+      expect( accounts0.length ).to.equal( 1 ) ;
+
+      const accounts1 = await bankAccount.connect( addr1 ).getAccounts() ;
+      expect( accounts1.length ).to.equal( 1 ) ;
+
+      const accounts2 = await bankAccount.connect( addr2 ).getAccounts() ;
+      expect( accounts2.length ).to.equal( 1 ) ;
+    } ) ;
+
+    it ( "should allow creating a quad user account", async () => {
+      const { bankAccount, addr0, addr1, addr2, addr3 } = await loadFixture( deployBankAccount ) ;
+      await bankAccount.connect( addr0 ).createAccount( [addr1.address, addr2.address, addr3.address] ) ;
+
+      const accounts0 = await bankAccount.connect( addr0 ).getAccounts() ;
+      expect( accounts0.length ).to.equal( 1 ) ;
+
+      const accounts1 = await bankAccount.connect( addr1 ).getAccounts() ;
+      expect( accounts1.length ).to.equal( 1 ) ;
+
+      const accounts2 = await bankAccount.connect( addr2 ).getAccounts() ;
+      expect( accounts2.length ).to.equal( 1 ) ;
+      
+      const accounts3 = await bankAccount.connect( addr3 ).getAccounts() ;
+      expect( accounts3.length ).to.equal( 1 ) ;
+    } ) ;
+
+    it ( "should not allow creating an account with duplicate owners", async () => {
+      const { bankAccount, addr0 } = await loadFixture( deployBankAccount ) ;
+      await expect( bankAccount.connect( addr0 ).createAccount( [addr0.address] ) ).to.be.reverted ;
+    } ) ;
+
+    it ( "should not allow creating an account with 5 owners", async () => {
+      const { bankAccount, addr0, addr1, addr2, addr3, addr4 } = await loadFixture( deployBankAccount ) ;
+      await expect( bankAccount.connect( addr0 ).createAccount( [
+        addr1.address, addr2.address, addr3.address, addr4.address
+      ] ) ).to.be.reverted ;
+    } ) ;
+    
+    it ( "should not allow a user have more than 3 accounts", async () => {
+      const { bankAccount, addr0 } = await loadFixture( deployBankAccount ) ;
+      for ( let i = 0 ; i < 3 ; i ++ ) {
+        await bankAccount.connect( addr0 ).createAccount( [] ) ;
+      } // for
+
+      await expect( bankAccount.connect( addr0 ).createAccount( [] ) ).to.be.reverted ;
+    } ) ;    
+  } ) ;
+
+  describe( "Depositing", () => {
+    it ( "should allow deposit from account owner", async () => {
+      const { bankAccount, addr0 } = await deployBankAccountWithAccounts( 1 ) ;
+      await expect( bankAccount.connect( addr0 ).deposit( 0, { value: "100" } ) )
+        .to.changeEtherBalances( [bankAccount, addr0], ["100", "-100"] ) ;
+      // bankAccount 這個智能合約擁有的 balance 增加 100
+      // addr0 的帳戶 balance 減少 100
+    } ) ;
+
+    it ( "should NOT allow deposit from non-account owner", async () => {
+      const { bankAccount, addr1 } = await deployBankAccountWithAccounts( 1 ) ;
+      await expect( bankAccount.connect( addr1 ).deposit( 0, { value: "100" } ) )
+        .to.be.reverted ;
+    } ) ;
+  } ) ;
+
+  describe( "Withdraw", () => {
+    describe( "Request a withdraw", () => {
+      it ( "account owner can request withdraw", async () => {
+        const { bankAccount, addr0 } = await deployBankAccountWithAccounts( 1, 100 ) ;
+        await bankAccount.connect( addr0 ).requestWithdraw( 0, 100 ) ;
+        // 這個測試不需要加 expect，因為如果 requestWithdraw 失敗便會 reverted
+        // 該測試便會失敗
+      } ) ;
+
+      it ( "account owner can not request withdraw with invalid amount", async () => {
+        const { bankAccount, addr0 } = await deployBankAccountWithAccounts( 1, 100 ) ;
+        await expect( bankAccount.connect( addr0 ).requestWithdraw( 0, 101 ) )
+          .to.be.reverted ;
+      } ) ;
+
+      it ( "non-account owner cannot request withdraw", async () => {
+        const { bankAccount, addr1 } = await deployBankAccountWithAccounts( 1, 100 ) ;
+        await expect( bankAccount.connect( addr1 ).requestWithdraw( 0, 90 ) )
+          .to.be.reverted ;
+      } ) ;
+
+      it ( "account owner can request withdraw multiple times", async () => {
+        const { bankAccount, addr0 } = await deployBankAccountWithAccounts( 1, 100 ) ;
+        await bankAccount.connect( addr0 ).requestWithdraw( 0, 90 ) ;
+        await bankAccount.connect( addr0 ).requestWithdraw( 0, 10 ) ;
+      } ) ;
+    } ) ;
+
+    describe( "Approve a withdraw", () => {
+      it ( "should allow account owner to approve withdraw", async () => {
+        const { bankAccount, addr1 } = await deployBankAccountWithAccounts( 2, 100, [100] ) ;
+        await bankAccount.connect( addr1 ).approveWithdraw( 0, 0 ) ;
+        expect( await bankAccount.getApprovals( 0, 0 ) ).to.equal( 1 ) ;
+      } ) ;
+
+      it ( "should not allow non-account owner to approve withdraw", async () => {
+        const { bankAccount, addr2 } = await deployBankAccountWithAccounts( 2, 100, [100] ) ;
+        await expect( bankAccount.connect( addr2 ).approveWithdraw( 0, 0 ) ).to.be.reverted ;
+      } ) ;
+
+      it ( "should not allow owner to approve withdraw multiple times", async () => {
+        const { bankAccount, addr1 } = await deployBankAccountWithAccounts( 2, 100, [100] ) ;
+        await bankAccount.connect( addr1 ).approveWithdraw( 0, 0 )
+        await expect( bankAccount.connect( addr1 ).approveWithdraw( 0, 0 ) ).to.be.reverted ;
+      } ) ;
+
+      it ( "should not allow creator of request to approve request", async () => {
+        const { bankAccount, addr0 } = await deployBankAccountWithAccounts( 2, 100, [100] ) ;
+        await expect( bankAccount.connect( addr0 ).approveWithdraw( 0, 0 ) ).to.be.reverted ;
+      } ) ;
+    } ) ;
+
+    describe( "Make withdraw", () => {
+      it ( "should allow creator of request to withdraw approved request", async () => {
+        const { bankAccount, addr0, addr1 } = await deployBankAccountWithAccounts( 2, 100, [100] ) ;
+        await bankAccount.connect( addr1 ).approveWithdraw( 0, 0 ) ;
+        await expect( bankAccount.connect( addr0 ).withdraw( 0, 0 ) )
+          .to.changeEtherBalances( [bankAccount, addr0], ["-100", "100"] ) ;
+      } ) ;
+
+      it ( "should not allow creator of request to withdraw approved request twice", async () => {
+        const { bankAccount, addr0, addr1 } = await deployBankAccountWithAccounts( 2, 200, [100] ) ;
+        await bankAccount.connect( addr1 ).approveWithdraw( 0, 0 ) ;
+        await expect( bankAccount.connect( addr0 ).withdraw( 0, 0 ) )
+          .to.changeEtherBalances( [bankAccount, addr0], ["-100", "100"] ) ;
+        await expect( bankAccount.connect( addr0 ).withdraw( 0, 0 ) )
+          .to.be.reverted ;
+        // 不是因為不足夠的 balance 導致失敗，而是因為已經用這個 approved request 取款過
+        // 不能利用這個 approved request 再做一次取款動作
+      } ) ;
+
+      it ( "should not allow non-creator of request to withdraw approved request", async () => {
+        const { bankAccount, addr1 } = await deployBankAccountWithAccounts( 2, 100, [100] ) ;
+        await bankAccount.connect( addr1 ).approveWithdraw( 0, 0 ) ;
+        await expect( bankAccount.connect( addr1 ).withdraw( 0, 0 ) )
+          .to.be.reverted ;
+      } ) ;
+
+      it ( "should not allow creator of request to withdraw approved request", async () => {
+        const { bankAccount, addr0 } = await deployBankAccountWithAccounts( 2, 100, [100] ) ;
+        await expect( bankAccount.connect( addr0 ).withdraw( 0, 0 ) )
+          .to.be.reverted ;
+      } ) ;
+    } ) ;
+  } ) ;
+} ) ;
